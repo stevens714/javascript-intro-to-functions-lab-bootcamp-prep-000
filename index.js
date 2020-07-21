0d667fc767f9926bc0aa694cf04de83eb41b6c4e
Steven Stieglitz <stevens714@yahoo.com>
2020-07-21 17:09:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ function shout(string) {return string.toUpperCase()}
 
 function whisper(string) {return string.toLowerCase()}
 
-function logshout(string) {console.log toUpperCase()}
-
+function logshout(string) {console.log (shout(string))}

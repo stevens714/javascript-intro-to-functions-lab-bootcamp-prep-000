8637f927c8435ccfe9508a1b5fb0aa5cf1e57bae
Steven Stieglitz <stevens714@yahoo.com>
2020-07-21 17:12:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ function whisper(string) {return string.toLowerCase()}
 function logShout(string) {
 
   console.log(shout(string));}
+
+function logWhisper(string) {
+  console.log(whisper(string));}
